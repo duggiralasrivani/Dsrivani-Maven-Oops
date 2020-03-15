35f9e16a2bda08e322be6142f01e9ea4c59cfc71
duggiralasrivani <srivani3299@gmail.com>
2020-03-15 22:38:36 +0530

    maven commit done

---
+++ Maven-oops/src/main/java/Sweety.java
@@ -0,0 +1,15 @@
+
+public class Sweety {
+   int weight,price;
+   Sweety(int weight,int price)
+   {
+	   this.weight=weight;
+	   this.price=price;
+   }
+   public int getPrice() {
+		return price;
+	}
+	public int getWeight() {
+		return weight;
+	}
+}
